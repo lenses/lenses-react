cd28749a183e4d060e264e70f4ddb582fcd482f0
Nisha Batra <nishabatra27@gmail.com>
2015-12-07 23:24:25 -0500

    set up gulp

--- server/main.js
+++ server/main.js
@@ -2,14 +2,11 @@ var express = require('express');
 
 var app = new express();
 
-// app.set('views', __dirname + '/app');
-// app.set('view engine', 'html');
-
 app.set('views', __dirname + '/app');
 app.engine('html', require('consolidate').handlebars);
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 
 app.get('/', function(req,res){
-  res.render('../../app/index.html', {});
+  res.render('../../app/index.ejs', {});
 })
 .listen(7777);
