f7f6904d2224df263cf644094b77ae1010908966
Nisha Batra <nishabatra27@gmail.com>
2015-12-07 23:06:21 -0500

    add dependencies for handlebars/html modules

--- server/main.js
+++ server/main.js
@@ -2,7 +2,14 @@ var express = require('express');
 
 var app = new express();
 
+// app.set('views', __dirname + '/app');
+// app.set('view engine', 'html');
+
+app.set('views', __dirname + '/app');
+app.engine('html', require('consolidate').handlebars);
+app.set('view engine', 'html');
+
 app.get('/', function(req,res){
-  res.render('./../app/index.html', {});
+  res.render('../../app/index.html', {});
 })
 .listen(7777);
