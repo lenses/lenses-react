6d5e466981af0e35cbb006a4d85161d131c89278
Nisha Batra <nishabatra27@gmail.com>
2015-12-08 00:10:12 -0500

    integrate browserify

--- server/main.js
+++ server/main.js
@@ -2,11 +2,11 @@ var express = require('express');
 
 var app = new express();
 
-app.set('views', __dirname + '/app');
-app.engine('html', require('consolidate').handlebars);
-app.set('view engine', 'ejs');
+app.set('views', __dirname + '../app');
 
 app.get('/', function(req,res){
   res.render('../../app/index.ejs', {});
 })
+.use(express.static(__dirname + '/../.tmp'))
 .listen(7777);
+
