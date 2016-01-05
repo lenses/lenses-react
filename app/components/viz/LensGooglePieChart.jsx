05b21da96d2cfa1ce6780334f5c1dc2b26247210
Kareem Amin <kareem@useframe.com>
2016-01-05 18:52:17 -0500

    adding a lens google piechart

---
+++ app/components/viz/LensGooglePieChart.jsx
@@ -0,0 +1,49 @@
+var React = require('react');
+
+var LensGoogleBarGraph = React.createClass({
+  drawChart: function() {
+      var dt = new window.google.visualization.DataTable();
+      var columns = this.props.columns;
+      var data = this.props.data;
+
+      columns.forEach(function(column){
+        var type = column[0];
+        var name = column[1];
+        dt.addColumn(type, name);
+      })
+
+      dt.addRows(data);
+
+      // Set chart options
+      var options = {'title':'How Much Pizza I Ate Last Night',
+        'width':600,
+        'height':400};
+
+      // Instantiate and draw our chart, passing in some options.
+      var chart = new window.google.visualization.PieChart(document.getElementById('chart-div'));
+      chart.draw(dt, options);
+  },
+  componentDidMount: function() {
+    // Inject google viz jsapi
+    // This is the method to load external dependencies when the component mounts
+    var script = document.createElement('script');
+    script.type="text/javascript";
+    var drawChart = this.drawChart;
+    script.onload = (function() {
+      window.google.load('visualization', '1.0',
+                         {packages:['corechart'],
+                           callback:drawChart
+                         });
+    });
+    (document.getElementsByTagName( "head" )[ 0 ]).appendChild( script );
+    script.src="https://www.google.com/jsapi";
+  },
+  render: function() {
+    return (
+      <div id='chart-div'>
+      </div>
+    )
+  }
+});
+
+module.exports = LensGoogleBarGraph;
