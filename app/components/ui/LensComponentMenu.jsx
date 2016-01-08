845ba8d0b7dc6b7a0dade2a7381508baa6e6bf9a
Kareem Amin <kareem@useframe.com>
2016-01-08 11:53:00 -0500

    finished interactoin and styling for adding custom compontnets on the fly

--- app/components/ui/LensComponentMenu.jsx
+++ app/components/ui/LensComponentMenu.jsx
@@ -5,7 +5,8 @@ var LensOvalButton = require('./LensOvalButton');
 var LensComponentMenu = React.createClass({
   getInitialState: function() {
     return {
-      value: "Enter Component Name"
+      value: "Enter Component Name",
+      inputField: false
     }
   },
   handleInputChange: function(e) {
@@ -13,9 +14,20 @@ var LensComponentMenu = React.createClass({
       value: e.target.value
     })
   },
-
+  toggleInputFields: function() {
+    if (this.state.inputField) {
+      this.setState({inputField: false});
+    } else {
+      this.setState({inputField: true});
+    }
+  },
+  addCustomComponent: function(componentName) {
+    this.toggleInputFields();
+    this.props.addCustomComponent(componentName);
+  },
   render: function() {
     var lensComponents = [];
+    var inputFields = [];
 
     if(this.props.lensComponentLibrary) {
       this.props.lensComponentLibrary.forEach(function(component, id) {
@@ -28,17 +40,39 @@ var LensComponentMenu = React.createClass({
           margin='5px'/>);
       }, this);
     }
+
+    if(this.state.inputField) {
+      inputFields = [];
+      inputFields.push(<input id='add-component-value'
+        type='text'
+        value={this.state.value}
+        onChange={this.handleInputChange}/>);
+      inputFields.push( <LensOvalButton key='submit-new-component'
+        margin='5px'
+        action={this.addCustomComponent}
+        actionPayload={this.state.value}
+        content='DONE' />)
+      inputFields.push( <LensOvalButton key='cancel-new-component'
+        margin='5px'
+        action={this.toggleInputFields}
+        actionPayload={null}
+        content='CANCEL' />)
+    } else {
+      inputFields = [];
+      inputFields.push(<LensOvalButton key='add-new-component'
+        margin='5px'
+        action={this.toggleInputFields}
+        actionPayload={null}
+        content='ADD NEW COMPONENT' />)
+    }
+
     return (
       <div className='lens-component-menu'>
         {lensComponents}
-        <LensOvalButton key='add-new-component'
-                        margin='5px'
-                        action={this.props.addCustomComponent}
-                        actionPayload={this.state.value}
-                        content='ADD NEW COMPONENT' />
-        <input id='add-component-value' type='text' value={this.state.value} onChange={this.handleInputChange}/>
+        {inputFields}
+
       </div>
-           )
+    )
   }
 });
 
