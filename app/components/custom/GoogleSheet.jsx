79985b0f2a9d713dcb2be733137d0f592d0fd804
Kareem Amin <kareem@useframe.com>
2016-01-09 16:39:33 -0500

    Adding functionality to the Google Sheet Component to retrieve info from a public spreadsheet and make it available in the correct format

--- app/components/custom/GoogleSheet.jsx
+++ app/components/custom/GoogleSheet.jsx
@@ -1,36 +1,72 @@
 var React = require('react');
-var $ = require('jquery');
 var tabletop = require('tabletop');
 
 var GoogleSheet = React.createClass({
   getInitialState: function() {
     return {
-      value: ""
+      value: "",
+      data: [],
+      columns: [],
+      tbInstance: null
     }
   },
-  handleInput: function() {
+  handleInputChange: function(e) {
+    this.setState({
+      value: e.target.value
+    });
   },
-  handleKeyDown: function () {
+  handleKeyDown: function (e) {
+    if (e.keyCode == 13) {
+      this.getGoogleSheetData();
+    }
+  },
+  getGoogleSheetData: function() {
+    tabletop.init({
+      key: this.state.value,
+      callback: this.processData,
+      simpleSheet: true
+    })
+  },
+  processData: function(data, tbInstance) {
+    var columns = [], tempData = [];
+    for(var x = 0; x < data.length; x++) {
+      var tempColumnData = [];
+      for (var column in data[x]) {
+        if(columns.indexOf(column) == -1){
+          columns.push(column);
+        }
+        tempColumnData.push(data[x][column]);
+      }
+      tempData.push(tempColumnData);
+    }
+    this.setState({
+      data: tempData,
+      columns: columns,
+      tbInstance: tbInstance
+    });
   },
   componentDidMount: function() {
     // This is the method to load external dependencies when the component mounts
     // React automagically bind this to method calls on the component so you can use
     // this safely in callback functions to refer to component methods
     // $.getScript("https://www.google.com/jsapi").done(this.callbackMethod);
-    tabletop.init( { key: '0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE',
-                  callback: function(data, tabletop) { console.log(data) },
-                  simpleSheet: true } );
   },
   render: function() {
     return (
-      <div class='google-sheet'>
-        inputFields.push(<input class='google-sheet'
-          type='text'
-          value={this.state.value}
-          placeholder="COMPONENT NAME"
-          onChange={this.handleInputChange}
-          onKeyDown={this.handleKeyDown}
-        />);
+      <div className='google-sheet'>
+        Enter the ID of a published Google Spreadsheet
+        <a href='#'>Follow Instructions here</a>
+        <div>
+          <input className='google-sheet'
+            type='text'
+            value={this.state.value}
+            placeholder="ENTER SHEET ID"
+            onChange={this.handleInputChange}
+            onKeyDown={this.handleKeyDown}
+          />
+        </div>
+        <div>Columns: {this.state.columns}</div>
+        <div>Data: {this.state.data}</div>
       </div>
     )
   }
