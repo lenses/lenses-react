4a8be2aac2bf3999ca01bacbfaac7289f1ee9597
Kareem Amin <kareem@useframe.com>
2016-01-14 15:14:45 -0500

    add more info to component template

--- app/components/custom/ComponentTemplate.jsx
+++ app/components/custom/ComponentTemplate.jsx
@@ -2,10 +2,41 @@ var React = require('react');
 // var $ = require('jquery');
 
 module.exports = React.createClass({
-  // this.props.data [[], []] row => data1, data2, data3
-  // E.g. [[1,2,3] , [4,5,6]]
-  // this.props.columns [[], []] type, column_name call it schema
-  // ['string', 'name'] , ['number' , 'age']
+  // Lenses works with Tabular Data
+  //
+  // If the data looks like this:
+  //
+  // |   Pizza    | Quantity | Company  |
+  // |------------|----------|----------|
+  // | Pepperoni  |        5 | PizzaHut |
+  // | Garlic     |        4 | Dominos  |
+  // | Everything |        1 | PizzaHut |
+  // | Garlic     |        5 | Dominos  |
+  //
+  //
+  // You can access the data and the dataSchema in Lenses in the following way:
+  //
+  // this.props.dataSchema =
+  // [
+  //  ['string', 'Pizza'],
+  //  ['number', 'Quantity'],
+  //  ['string', 'Company']
+  // ]
+  //
+  // The Data Schema is an arrary of arrays that represents the type of data in a column and the column header.
+  // The input component determines how to parse the data to figure out the type of data in the column. It should
+  // take a best guess but could be incorrect and should allow users to change that.
+  //
+  //
+  // this.props.data =
+  // [
+  //  ['Pepperoni', 5, 'PizzaHut'],
+  //  ['Garlic', 4, 'Dominos'],
+  //  ['Everything', 1, 'PizzaHut'],
+  //  ['Garlic', 5, 'Dominos'],
+  // ]
+  //
+  // The Data is an array of arrays that represents each row of the tabular data and a value for each column.
   getInitialState: function() {
     // return an object of name value pairs where each name is a configurable variable
     // that you have set up in getCustomOptions and each value is the initial value for that variable.
@@ -31,13 +62,13 @@ module.exports = React.createClass({
     // Do your drawing or data manipulations in any method that you define in this
     // object.
     // If your component returns manipulated data then make sure to call
-    // this.props.updateTransformFunction(yourTransformFunction)
+    // this.props.updateTransformFunction(yourTransformFunction(), dataSchema)
   },
   yourTransformFunction: function() {
-    // make sure to call this.props.updateDataSchema(newDataSchema);
-    // this will update your dataschema wen you update your data
     // the transform function should not contain any async calls. If you need to make async calls
     // make them before hand and pass the result to this function
+    // It should pass the data in a closure to be used later. i.e. you should return a function here that
+    // returns your newly transformed or retrieved data
   },
   componentDidUpdate: function() {
     // If you are creating a drawing component make sure to call your drawing function here
